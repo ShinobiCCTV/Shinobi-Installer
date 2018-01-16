9a08e0f2f8cf227895b056dda53b2c0079d46cb8
Moe <github@m03.ca>
2018-01-15 16:28:09 -0800

    support centos and mac

--- shinobi-install.sh
+++ shinobi-install.sh
@@ -3,8 +3,23 @@ if ((EUID != 0)); then
   echo >&2 "Shinobi requires that it be run as root because PM2 is used to manage the processes. Exiting..."
   exit 1
 fi
-apt update
-apt install git -y
+if [ ! -x "$(command -v git)" ]; then
+    if [ -x "$(command -v apt)" ]; then
+        apt update
+        apt install git -y
+    fi
+    if [ -x "$(command -v yum)" ]; then
+        yum update
+        yum install git -y
+    fi
+    if [ "$OSTYPE" == "darwin"* ]; then
+        if [ -x "$(command -v brew)" ]; then
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            brew doctor
+        fi
+        brew install git
+    fi
+fi
 cd /home
 echo "Install the Development branch?"
 echo "(y)es or (N)o"
