54fd1a2ddc8af8d393a123841c877e0f15f37a02
Moe <github@m03.ca>
2018-01-16 11:56:52 -0800

    fix mac rules, add choice for CE or Pro

--- shinobi-install.sh
+++ shinobi-install.sh
@@ -1,37 +1,48 @@
 #/bin/sh
-if ((EUID != 0)); then
-  echo >&2 "Shinobi requires that it be run as root because PM2 is used to manage the processes. Exiting..."
-  exit 1
-fi
-if [ ! -x "$(command -v git)" ]; then
-    if [ -x "$(command -v apt)" ]; then
-        apt update
-        apt install git -y
+if [ "$OSTYPE" == "darwin"* ] && [ ! -x "$(command -v git)" ]; then
+    if [ ! -x "$(command -v brew)" ]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        brew doctor
     fi
-    if [ -x "$(command -v yum)" ]; then
-        yum update
-        yum install git -y
+    brew install git
+else
+    if ((EUID != 0)); then
+      echo >&2 "Shinobi requires that it be run as root because PM2 is used to manage the processes. Exiting..."
+      exit 1
     fi
-    if [ "$OSTYPE" == "darwin"* ]; then
-        if [ -x "$(command -v brew)" ]; then
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-            brew doctor
+    if [ ! -x "$(command -v git)" ]; then
+        if [ -x "$(command -v apt)" ]; then
+            apt update
+            apt install git -y
         fi
-        brew install git
+        if [ -x "$(command -v yum)" ]; then
+            yum update
+            yum install git -y
+        fi
     fi
 fi
 cd /home
-echo "Install the Development branch?"
-echo "(y)es or (N)o"
-read theBranchChoice
-if [ "$theBranchChoice" = "Y" ] || [ "$theBranchChoice" = "y" ]; then
-    echo "Getting the Development Branch"
-    theBranch='dev'
-else
-    echo "Getting the Grand Master Branch"
+echo "Install Shinobi CE or Shinobi Pro?"
+echo "*Note : Shinobi Pro is free for personal use."
+echo "(C)E or (P)ro? Default : Pro"
+read theRepoChoice
+if [ "$theRepoChoice" = "C" ] || [ "$theRepoChoice" = "c" ]; then
+    theRepo='ShinobiCCTV'
     theBranch='master'
+else
+    theRepo='ShinobiCCTV'
+    echo "Install the Development branch?"
+    echo "(y)es or (N)o"
+    read theBranchChoice
+    if [ "$theBranchChoice" = "Y" ] || [ "$theBranchChoice" = "y" ]; then
+        echo "Getting the Development Branch"
+        theBranch='dev'
+    else
+        echo "Getting the Grand Master Branch"
+        theBranch='master'
+    fi
 fi
-git clone https://github.com/ShinobiCCTV/Shinobi.git -b $theBranch
+git clone https://github.com/$theRepo/Shinobi.git -b $theBranch
 cd Shinobi
 chmod +x INSTALL/start.sh
 INSTALL/start.sh
