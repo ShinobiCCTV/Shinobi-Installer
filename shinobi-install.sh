fb3544ea48e0cc8e8dced682623cf911bd06de29
Moe <github@m03.ca>
2018-01-16 22:17:08 -0800

    allow custom location for install

--- shinobi-install.sh
+++ shinobi-install.sh
@@ -1,90 +1,124 @@
 #! /bin/sh
-case "$(uname -s)" in
-   Darwin)
-     OSTYPE='darwin'
-     ;;
-   Linux)
-     OSTYPE='linux'
-     ;;
-esac
-if [ "$OSTYPE" = "darwin"* ] && [ ! -x "$(command -v git)" ]; then
-    if [ ! -x "$(command -v brew)" ]; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew doctor
-    fi
-    brew install git
-else
-    if [ "$(id -u)" != 0 ]; then
-        echo "*--------------------**---------------------*"
-        echo "*Shinobi requires being run as root."
-        echo "*Do you want to continue without being root?"
-        echo "(Y)es or (n)o? Default : Yes"
-        read nonRootUser
-        if [  "$nonRootUser" = "N" ] || [  "$nonRootUser" = "n" ]; then
-            echo "Stopping..."
-            exit 1
-        fi
-    fi
-    if [ ! -x "$(command -v git)" ]; then
-        if [ -x "$(command -v apt)" ]; then
-            sudo apt update
-            sudo apt install git -y
-        fi
-        if [ -x "$(command -v yum)" ]; then
-            sudo yum update
-            sudo yum install git -y
-        fi
-    fi
-fi
-cd /home
-echo "*--------------------**---------------------*"
-echo "Install Shinobi CE or Shinobi Pro?"
 echo "---------------------------------------------"
-echo "*Note : Shinobi Pro is free for personal use."
-echo "*Learn more at http://shinobi.video/pro"
-echo "---------------------------------------------"
-echo "(C)E or (P)ro? Default : Pro"
-read theRepoChoice
-if [ "$theRepoChoice" = "C" ] || [ "$theRepoChoice" = "c" ] || [ "$theRepoChoice" = "CE" ] || [ "$theRepoChoice" = "ce" ]; then
-    productName="Shinobi Community Editon (CE)"
-    theRepo='moeiscool'
-    theBranch='master'
-else
-    theRepo='ShinobiCCTV'
-    productName="Shinobi Professional (Pro)"
-    echo "Install the Development branch?"
-    echo "(y)es or (N)o? Default : No"
-    read theBranchChoice
-    if [ "$theBranchChoice" = "Y" ] || [ "$theBranchChoice" = "y" ]; then
-        echo "Getting the Development Branch"
-        theBranch='dev'
+echo "Install Location for Shinobi"
+echo '*Note : Default install location is "/home"'
+echo "Do you want to install a custom location for Shinobi?"
+echo "(y)es or (N)o? Default : No"
+    read installLocationChoice
+    if [ "$installLocationChoice" = "Y" ] || [ "$installLocationChoice" = "y" ]; then
+        echo "Example : /home"
+        read installLocation
     else
-        echo "Getting the Master Branch"
-        theBranch='master'
+        installLocation='/home'
     fi
-fi
-# Download from Git repository
-gitURL="https://github.com/$theRepo/Shinobi"
-sudo git clone $gitURL.git -b $theBranch
-# Enter Shinobi folder "/home/Shinobi"
-cd Shinobi
-# write version number
-function getGitVersionNumber() {
-  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
-}
-gitVersionNumber=$(getGitVersionNumber)
-theDateRightNow=$(date)
-cat <<EOF > version.json
-{"Product" : "$productName" , "Branch" : "$theBranch" , "Version" : "$gitVersionNumber" , "Date" : "$theDateRightNow" , "Repository" : "$gitURL"}
-EOF
-echo "-------------------------------------"
-echo "---------- Shinobi Systems ----------"
-echo "Repository : $gitURL"
-echo "Product : $productName"
-echo "Branch : $theBranch"
-echo "Version : $gitVersionNumber"
-echo "Date : $theDateRightNow"
-echo "-------------------------------------"
-echo "-------------------------------------"
+cd $installLocation
+echo "Opening Install Location : \"$installLocation\""
+#if [ ! -d "Shinobi" ]; then
+    # Check OS
+    case "$(uname -s)" in
+       Darwin)
+       # Mac OS
+         OSTYPE='darwin'
+         ;;
+       Linux)
+       # Ubuntu, CentOS
+         OSTYPE='linux'
+         ;;
+    esac
+    # Check if Mac OS and if Git is needed
+    if [ "$OSTYPE" = "darwin"* ] && [ ! -x "$(command -v git)" ]; then
+        if [ ! -x "$(command -v brew)" ]; then
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            brew doctor
+        fi
+        brew install git
+    else
+        # Check if user is root
+        if [ "$(id -u)" != 0 ]; then
+            echo "*--------------------**---------------------*"
+            echo "*Shinobi requires being run as root."
+            echo "*Do you want to continue without being root?"
+            echo "(Y)es or (n)o? Default : Yes"
+            read nonRootUser
+            if [  "$nonRootUser" = "N" ] || [  "$nonRootUser" = "n" ]; then
+                echo "Stopping..."
+                exit 1
+            fi
+        fi
+        # Check if Git is needed
+        if [ ! -x "$(command -v git)" ]; then
+            # Check if Ubuntu
+            if [ -x "$(command -v apt)" ]; then
+                sudo apt update
+                sudo apt install git -y
+            fi
+            # Check if Cent OS
+            if [ -x "$(command -v yum)" ]; then
+                sudo yum update
+                sudo yum install git -y
+            fi
+        fi
+    fi
+    echo "*--------------------**---------------------*"
+    echo "Install Shinobi CE or Shinobi Pro?"
+    echo "---------------------------------------------"
+    echo "*Note : Shinobi Pro is free for personal use."
+    echo "*Learn more at http://shinobi.video/pro"
+    echo "---------------------------------------------"
+    echo "(C)E or (P)ro? Default : Pro"
+    read theRepoChoice
+    if [ "$theRepoChoice" = "C" ] || [ "$theRepoChoice" = "c" ] || [ "$theRepoChoice" = "CE" ] || [ "$theRepoChoice" = "ce" ]; then
+        productName="Shinobi Community Editon (CE)"
+        theRepo='moeiscool'
+        theBranch='master'
+    else
+        theRepo='ShinobiCCTV'
+        productName="Shinobi Professional (Pro)"
+        echo "Install the Development branch?"
+        echo "(y)es or (N)o? Default : No"
+        read theBranchChoice
+        if [ "$theBranchChoice" = "Y" ] || [ "$theBranchChoice" = "y" ]; then
+            echo "Getting the Development Branch"
+            theBranch='dev'
+        else
+            echo "Getting the Master Branch"
+            theBranch='master'
+        fi
+    fi
+    # Download from Git repository
+    gitURL="https://github.com/$theRepo/Shinobi"
+    sudo git clone $gitURL.git -b $theBranch
+    # Enter Shinobi folder "/home/Shinobi"
+    cd Shinobi
+    # get version number
+    function getGitVersionNumber() {
+      git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+    }
+    gitVersionNumber=$(getGitVersionNumber)
+    theDateRightNow=$(date)
+    # write the version.json file for the main app to use
+    cat <<EOF > version.json
+    {
+        "Product" : "$productName",
+        "Branch" : "$theBranch" ,
+        "Version" : "$gitVersionNumber" ,
+        "Date" : "$theDateRightNow" ,
+        "Repository" : "$gitURL"
+    }
+    EOF
+    echo "-------------------------------------"
+    echo "---------- Shinobi Systems ----------"
+    echo "Repository : $gitURL"
+    echo "Product : $productName"
+    echo "Branch : $theBranch"
+    echo "Version : $gitVersionNumber"
+    echo "Date : $theDateRightNow"
+    echo "-------------------------------------"
+    echo "-------------------------------------"
+#else
+#    echo "Shinobi already downloaded."
+#    cd Shinobi
+#fi
+# start the installer in the main app (or start shinobi if already installed)
 sudo chmod +x INSTALL/start.sh
 sudo INSTALL/start.sh
